43b0c9b8871142ea4006348ebfa204f7c8ed1629
IrynaDiatel <kastyrinaira@gmail.com>
2026-02-14 16:14:02 +0200

    error

--- gallery.js
+++ gallery.js
@@ -1,9 +1,9 @@
 const images = [
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820__480.jpg",
     original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820_1280.jpg",
     description: "Hokkaido Flower",
   },
   {
@@ -65,33 +65,36 @@ const images = [
 ];
 
 const gallery = document.querySelector(".gallery");
+
 const galleryMarkup = images
-  .map(({ preview, original, description }) => {
-    return `
-        <li class="gallery-item">
-          <a class="gallery-link" href="${original}">
-            <img
-              class="gallery-image"
-              src="${preview}"
-              data-source="${original}"
-              alt="${description}"
-            />
-          </a>
-        </li>
-      `;
-  })
+  .map(
+    ({ preview, original, description }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `,
+  )
   .join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
 gallery.addEventListener("click", (event) => {
-  event.preventDefault();
   if (event.target.nodeName !== "IMG") return;
 
-  const source = event.target.dataset.source;
+  event.preventDefault();
+
+  const largeImageURL = event.target.dataset.source;
 
   const instance = basicLightbox.create(`
-        <img src="${source}" width="1112" height="640">
-      `);
+    <img src="${largeImageURL}" alt="${event.target.alt}">
+  `);
+
   instance.show();
 });
